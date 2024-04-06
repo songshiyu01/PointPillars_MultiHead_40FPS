8f399a61bbc5db94e2437151242eaf545f4c196f
Shiyu Song <shiyus@safeai.ai>
2024-04-06 18:03:48 +0000

    Changes in pcd_viewer.py

--- tools/pcd_viewer.py
+++ tools/pcd_viewer.py
@@ -4,6 +4,7 @@ from visual_tools import draw_clouds_with_boxes
 import open3d as o3d
 import yaml
 import sys
+import pathlib
 
 def dataloader(cloud_path , boxes_path):
     pcd = o3d.io.read_point_cloud(cloud_path)
@@ -14,8 +15,11 @@ def dataloader(cloud_path , boxes_path):
     return cloud,boxes
 
 if __name__ == "__main__":
-   pre_path = "/root/OpenPCDet/data/safeai_cache/v1.2-safeai-cache/samples/LIDAR_TOP_PLATE/"
-   pcd_path = pre_path + sys.argv[1] + ".pcd"
-   bb_path = pre_path + sys.argv[1] + ".txt"
-   cloud, boxes = dataloader(pcd_path, bb_path)
-   draw_clouds_with_boxes(cloud ,boxes)
+    if len(sys.argv) < 2:
+        print("Usage: " + sys.argv[0] + " path_to_data")
+        quit()
+    for pcd_file in pathlib.Path(sys.argv[1]).glob("*.pcd"):
+        pcd_path = pcd_file
+        bb_path = pcd_file.with_suffix(".txt")
+        cloud, boxes = dataloader(str(pcd_path), str(bb_path))
+        draw_clouds_with_boxes(cloud, boxes)
